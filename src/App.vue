50682d5ac3153b8f5fb38dce0fd2a76221a294f4
Onwunali Tochukwu <Onwunali91@yahoo.com>
2020-09-02 16:48:14 +0100

    First push on the project

---
+++ src/App.vue
@@ -0,0 +1,75 @@
+<template>
+  <div id="app">
+    <section class="landing">
+      <div class="heading">
+        <h2 class="logo">cardino web</h2>
+        <ul class="nav">
+          <li>Home</li>
+          <li>About</li>
+          <li>Process</li>
+          <li>Packages</li>
+          <li>Testimonials</li>
+          <li>Portfolio</li>
+          <li>Team</li>
+          <li>Contact</li>
+        </ul>
+        <a href="#">Get Started</a>
+      </div>
+      <div class="side-icons">
+        <img src="./assets/icons/facebook.svg" alt="socials">
+        <img src="./assets/icons/instagram.svg" alt="socials">
+        <img src="./assets/icons/linkedin.svg" alt="socials">
+        <img src="./assets/icons/twitter.svg" alt="socials">
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+.landing{
+  height: 500px;
+  width: 100%;
+  background-color: #0F101E;
+  color: #fff;
+  font-family: 'Roboto', sans-serif;
+}
+.heading{
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+}
+.nav{
+  display: flex;
+}
+.nav li{
+  padding: 20px;
+  list-style-type: none;
+}
+.heading a{
+  text-decoration: none;
+  padding: 6px;
+  border-bottom: 1px solid #fff;
+  color: #fff;
+}
+.side-icons{
+  display: flex;
+  flex-direction: column;
+  width: fit-content;
+  position: absolute;
+  left: 0px;
+  top: 150px;
+}
+.side-icons img{
+  padding: 10px 0px 30px 20px;
+}
+.logo{
+  padding-right: 25px;
+  border-right: 3px solid royalblue;
+}
+</style>
---
+++ src/styleguide.md
@@ -0,0 +1,37 @@
+Style Guide
+
+
+## Colors
+
+### Primary
+
+- UK Blue (heading underline, arrow colours, clientimg2 backdrop) : #1D3176
+- Really Dark Blue (hero background, whyus background): #0F101E
+- Some kind of red (arrows, clientimg1 backdrop, Getstarted button) : #F12C41
+- Grey (About us text, testimonial background, Portfolio heading, CTA/footer background) : #252626
+
+
+### Neutral
+
+- White: #FFFFFF
+- Black (I can't even remember if I used black, but just in case): #000000
+
+## Typography
+
+### Body Copy
+
+
+### Headings,... yeah just headings
+
+- Family: [Montserrat](https://fonts.google.com/specimen/Montserrat)
+- ### Weights
+- Hero : Extrabold (800) [the 'web agency' under the 'cardino' in the heading : Bold(700)]
+- Headings : Bold (700)
+
+
+### Body text, navigation and literally every other piece of text apart from Headings and Headers
+- Family: [Roboto](https://fonts.google.com/specimen/Roboto)
+- ### Weights
+- Navigation, Client title [testimonial], footer, buttons : Medium (500) 
+- Get started button [nav] : Bold (700)
+- About section, Testimonial, Why Us, CTA : Regular (400)
